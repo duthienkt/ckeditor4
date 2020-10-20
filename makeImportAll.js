5095b70b0e92d40b7abab32244f264c32b5cdab3
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-10-20 14:55:45 +0700

    - import language

--- makeImportAll.js
+++ makeImportAll.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
+global.CKEDITOR = {};
+require('./core/loader');
+require('./core/lang');
 var scripts = CKEDITOR.scripts;
+var languages = CKEDITOR.lang.languages;
 
 var loaded = {};
 
@@ -15,7 +19,12 @@ function importCode(name) {
 	code.push(`import './core/${name}';`)
 }
 
+function importLang(name){
+	code.push(`import './lang/${name}';`);
+}
 
 Object.keys(scripts).forEach(importCode);
+Object.keys(languages).forEach(importLang);
+
 
 fs.writeFileSync('importAll.js', code.join('\n'), 'utf8');
